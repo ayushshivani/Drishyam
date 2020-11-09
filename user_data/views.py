ee6ec838d6e351f958abeb0a14ff8838d2269f9d
Ayush Shivani <ashivani@aicrowd.com>
2020-11-09 11:35:08 +0530

    removed entry to database while commiting

--- user_data/views.py
+++ user_data/views.py
@@ -24,11 +24,11 @@ class UserDataEntry(APIView):
         file_data = json.dumps(file_data)
         file_data.encode('utf-8')
 
-        file_name = 'files/file_' + str(count) + '.txt'
-        count += 1
-        f = open(file_name,"a")
-        f.write(file_data)
-        f.close()
+        # file_name = 'files/file_' + str(count) + '.txt'
+        # count += 1
+        # f = open(file_name,"a")
+        # f.write(file_data)
+        # f.close()
         
         md5_returned = hashlib.md5(file_data.encode()).hexdigest()
         response_dict = {"MD5sum": md5_returned}
